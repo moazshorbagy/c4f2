b8f3739efa6b3424db809449475ce7c14c04ff77
Moaz Alaa El-Shorbagy <moaz_elshorbagy@hotmail.com>
2021-03-21 12:12:26 +0200

    Trivial agent added

--- Agent.py
+++ Agent.py
@@ -1,6 +1,21 @@
 import random
 
+INNER_VERT = [1, 2]
+INNER_HORI = [3, 4]
+OUTER_VERT = [5, 6]
+OUTER_HORI = [7, 8]
+THRESHOLD = 45
 
 class Agent:
     def select_action(self, state, conn=None, vehicle_ids=None):
-        return random.randint(0, 1)
+        if(state[INNER_VERT[0]] > THRESHOLD or state[INNER_VERT[1]] > THRESHOLD):
+            return 0
+        if(state[INNER_HORI[0]] > THRESHOLD or state[INNER_HORI[1]] > THRESHOLD):
+            return 1
+
+        if(sum([state[idx] for idx in OUTER_VERT]) > 0.2):
+            return 0
+        if(sum([state[idx] for idx in OUTER_HORI]) > 0.2):
+            return 1
+        
+        return state[0]
